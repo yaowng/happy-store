62f126f4e661468079c5fca1bb1a2abc7cd79d4c
Prasada <prasada.appdev@gmail.com>
2018-07-16 18:56:19 +0800

    login page + router guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './_guards/auth.guard';
+
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './account/login/login.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: '', pathMatch: 'full', redirectTo: '/home' }
+  // { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
